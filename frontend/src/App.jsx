27903c5675af791412b7bbffa02e6cd54d0dda50
alejandro <alhiperac500@gmail.com>
2025-10-14 17:35:03 -0500

    practica de react y tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LandingPage from './pages/LandingPage.jsx'
+
+
+
+
+
 
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold">Tailwind está funcionando 🎉</h1>
+        <h2 className="mt-5 border border-gray-300 rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300 p-6">
+          prueba dos
+        </h2>
+      </div>
+    </div>
   )
 }
 
