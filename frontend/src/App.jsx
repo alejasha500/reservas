670d585f25efdcfdc6bc9c8513ad3219121acd35
alejandro <alhiperac500@gmail.com>
2025-10-16 03:18:59 -0500

    maqueta del proyecto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 // src/App.jsx
-import Card from "./components/card"
+
 
 function App() {
   return (
     <div className="bg-gradient-to-br from-blue-400 to-black">
-       <Card/>  
+         
     </div>
   )
 }
